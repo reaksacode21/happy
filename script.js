8c197fc4cb894a7849e071a36def376a88b68c5f
reaksacode21 <reaksara497@gmail.com>
2024-02-09 21:30:54 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const showBlessingsBtn = document.getElementById('showBlessings');
+const blessingsDiv = document.getElementById('blessings');
+
+const blessings = [
+    'សូមជូនពរអ្នកកុំអោយគេបោកច្រើនពេក!',
+    'កុំបោកគេពេកពៅប្រយត្ន័គេបោកវិញ!',
+    'ជូនពរអោយមានសង្សារឆាប់!',
+    'សូមជូនពរអោយអ្នកកុំស្រលាញ់គេម្នាក់ឯង!',
+    'សូមជូនពរអោយអ្នកកុំអោយក្លាយធ្វើជាជនទី៣',
+    'សូមជូនពរអោយអ្នកកុំស្រលាញ់សង្សារគេ',
+    'មានសង្សារនៅ!',
+    'សុំTelegram:you can contact me:097 6765 427 មិនឆាតមកប្រយត្ន័គ្មានអ្នកស្រលាញ់'
+];
+
+showBlessingsBtn.addEventListener('click', () => {
+    const randomIndex = Math.floor(Math.random() * blessings.length);
+    blessingsDiv.innerHTML = `<p>${blessings[randomIndex]}</p>`;
+});
